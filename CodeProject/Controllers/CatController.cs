770bac13906e1788fadfe7d6e19dad6da97b25b1
Christian Caluya <christianlcaluya@iskolarngbayan.pup.edu.ph>
2024-10-19 22:13:17 +0800

    not finished lacking catfacts

---
+++ CodeProject/Controllers/CatController.cs
@@ -0,0 +1,98 @@
+﻿using CodeProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
+using System.Text;
+using Newtonsoft.Json;
+
+using System.Reflection;
+namespace CodeProject.Controllers
+{
+    public class CatController : Controller
+    {
+        // GET: CatController
+        public async Task<ActionResult> Index()
+        {
+            string apiUrl = "https://cat-fact.herokuapp.com/#/cat/facts";
+
+            List<Cats> quo = new List<Cats>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                var result = await response.Content.ReadAsStringAsync();
+                quo = JsonConvert.DeserializeObject<List<Cats>>(result);
+            }
+            return View(quo);
+        }
+        // GET: CatController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CatController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CatController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CatController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CatController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CatController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CatController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CodeProject/Models/Cats.cs
@@ -0,0 +1,8 @@
+﻿namespace CodeProject.Models
+{
+    public class Cats
+    {
+        public string Fact { get; set; }
+        public string User { get; set; }
+    }
+}
